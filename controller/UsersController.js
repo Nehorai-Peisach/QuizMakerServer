1fdf888e6a2feb24d8faf4735178d92dece7855e
Nehorai Peisach <87582902+Nehorai-Peisach@users.noreply.github.com>
2022-02-28 02:59:23 +0200

    finished

--- controller/UsersController.js
+++ controller/UsersController.js
@@ -9,6 +9,13 @@ module.exports = class UsersController {
     return await this.repo.getAllUsers();
   }
 
+  async getUserByDetails(user) {
+    const tmp = await this.repo.getAllUsers();
+    const result = tmp.find((x) => 
+      x._doc.username === user.username && x._doc.password === user.password
+    );
+    return result;
+  }
   // Add user to the list
   async addUser(user) {
     const tmpUser = { _id: new mongoose.Types.ObjectId(), ...user };
